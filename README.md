9dffcf024dbdc9b4afccc6be09557eb46eb76516
Árpád Goretity <h2co3@h2co3.org>
2018-07-12 20:12:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Avocado, the strongly-typed MongoDB driver
+
+[![Avocado on crates.io](https://img.shields.io/crates/v/avocado.svg)](https://crates.io/crates/avocado)
+[![Avocado on docs.rs](https://docs.rs/avocado/badge.svg)](https://docs.rs/avocado)
+[![Avocado Download](https://img.shields.io/crates/d/avocado.svg)](https://crates.io/crates/avocado)
+[![Avocado License](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/H2CO3/avocado/blob/master/LICENSE.txt)
+[![Lines of Code](https://tokei.rs/b1/github/H2CO3/avocado)](https://github.com/Aaronepower/tokei)
+[![Twitter](https://img.shields.io/badge/twitter-@H2CO3_iOS-blue.svg?style=flat&colorB=64A5DE&label=Twitter)](http://twitter.com/H2CO3_iOS)
---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+//! Avocado: the strongly-typed MongoDB driver
+
+#![doc(html_root_url = "https://docs.rs/avocado/0.1.0")]
+#![deny(missing_debug_implementations, missing_copy_implementations,
+        trivial_casts, trivial_numeric_casts,
+        unsafe_code,
+        unstable_features,
+        unused_import_braces, unused_qualifications, missing_docs)]
+#![cfg_attr(feature = "cargo-clippy",
+            allow(single_match, match_same_arms, match_ref_pats,
+                  clone_on_ref_ptr, needless_pass_by_value))]
+#![cfg_attr(feature = "cargo-clippy",
+            deny(wrong_pub_self_convention, used_underscore_binding,
+                 stutter, similar_names, pub_enum_variant_names,
+                 missing_docs_in_private_items,
+                 non_ascii_literal, unicode_not_nfc,
+                 result_unwrap_used, option_unwrap_used,
+                 option_map_unwrap_or_else, option_map_unwrap_or, filter_map,
+                 shadow_unrelated, shadow_reuse, shadow_same,
+                 int_plus_one, string_add_assign, if_not_else,
+                 invalid_upcast_comparisons,
+                 cast_precision_loss, cast_lossless,
+                 cast_possible_wrap, cast_possible_truncation,
+                 mutex_integer, mut_mut, items_after_statements,
+                 print_stdout, mem_forget, maybe_infinite_iter))]
