2331e701ac1c7b7f0e0719b049c06ab149ddd5cd
Árpád Goretity <h2co3@h2co3.org>
2018-08-02 19:46:36 +0200

    Add an `i64_to_usize_with_msg()` function

--- src/coll.rs
+++ src/coll.rs
@@ -13,23 +13,29 @@ use error::{ Error, Result, ResultExt };
 
 /// Converts an `i32` to a `usize` if the range and value permits.
 /// Constructs an error message based on `msg` otherwise.
-#[cfg_attr(feature = "cargo-clippy", allow(cast_possible_wrap, cast_possible_truncation, if_same_then_else))]
 fn i32_to_usize_with_msg(n: i32, msg: &str) -> Result<usize> {
+    i64_to_usize_with_msg(n.into(), msg)
+}
+
+/// Converts an `i64` to a `usize` if the range and value permits.
+/// Constructs an error message based on `msg` otherwise.
+#[cfg_attr(feature = "cargo-clippy", allow(cast_possible_wrap, cast_possible_truncation, if_same_then_else))]
+fn i64_to_usize_with_msg(n: i64, msg: &str) -> Result<usize> {
     use std::usize;
     use std::mem::size_of;
 
-    // XXX: the correctness of this usize -> i32 cast relies on the following:
-    // 1. if `sizeof(usize) >= sizeof(i32)`, i.e. 32-bit and wider word size
-    //    platforms (the typical), then `i32::MAX` always fits into a `usize`,
+    // XXX: the correctness of this usize -> i64 cast relies on the following:
+    // 1. if `sizeof(usize) >= sizeof(i64)`, i.e. 64-bit and wider word size
+    //    platforms (the typical), then `i64::MAX` always fits into a `usize`,
     //    therefore the cast `n as usize` is safe as long as `n >= 0`.
-    // 2. Otherwise, if `sizeof(usize) < sizeof(i32)`, eg. 16-bit architectures,
-    //    then we can safely cast `usize::MAX` to `i32` in order to find out
-    //    via comparison whether the actual `i32` value fits dynamically.
+    // 2. Otherwise, if `sizeof(usize) < sizeof(i64)`, eg. 32-bit architectures,
+    //    then we can safely cast `usize::MAX` to `i64` in order to find out
+    //    via comparison whether the actual `i64` value fits dynamically.
     if n < 0 {
         Err(Error::new(format!("{} ({}) is negative", msg, n)))
-    } else if size_of::<usize>() >= size_of::<i32>() {
+    } else if size_of::<usize>() >= size_of::<i64>() {
         Ok(n as usize)
-    } else if n <= usize::MAX as i32 {
+    } else if n <= usize::MAX as i64 {
         Ok(n as usize)
     } else {
         Err(Error::new(format!("{} ({}) overflows `usize`", msg, n)))
