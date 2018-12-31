08d1f5ea3a1590df859e4824bdd47082b662b3ef
Árpád Goretity <h2co3@h2co3.org>
2018-12-31 18:28:56 +0100

    Add an optional transformation function to `Cursor`; we will need it.

--- avocado/src/cursor.rs
+++ avocado/src/cursor.rs
@@ -4,26 +4,40 @@ use std::fmt;
 use std::iter::Iterator;
 use std::marker::PhantomData;
 use serde::Deserialize;
-use crate::{
-    bsn::*,
-    error::{ Result, ResultExt },
-};
+use bson::{ Bson, Document, from_bson };
+use crate::error::{ Result, ResultExt };
 
 /// A typed wrapper around the MongoDB `Cursor` type.
-pub struct Cursor<T> where T: for<'a> Deserialize<'a> {
+pub struct Cursor<T> {
     /// The underlying MongoDB cursor.
     inner: mongodb::cursor::Cursor,
+    /// The function applied to each returned `Document` before deserialization.
+    transform: fn(Document) -> Result<Bson>,
     /// Just here so that the type parameter is used.
     _marker: PhantomData<T>,
 }
 
 impl<T> Cursor<T> where T: for<'a> Deserialize<'a> {
+    /// Creates a strongly-typed cursor from an untyped MongoDB cursor
+    /// and a transformation function.
+    #[doc(hidden)]
+    pub fn from_cursor_and_transform(
+        inner: mongodb::cursor::Cursor,
+        transform: fn(Document) -> Result<Bson>,
+    ) -> Self {
+        Cursor {
+            inner,
+            transform,
+            _marker: PhantomData,
+        }
+    }
+
     /// Reads the remaining documents available in the current batch.
     pub fn next_batch(&mut self) -> Result<Vec<T>> {
         self.inner
             .drain_current_batch()
             .chain("couldn't retrieve next batch")
-            .and_then(deserialize_documents)
+            .and_then(|docs| self.transform_and_deserialize_many(docs))
     }
 
     /// Retrieves the next at most `n` documents.
@@ -31,12 +45,24 @@ impl<T> Cursor<T> where T: for<'a> Deserialize<'a> {
         self.inner
             .next_n(n)
             .chain("couldn't retrieve documents")
-            .and_then(deserialize_documents)
+            .and_then(|docs| self.transform_and_deserialize_many(docs))
     }
 
     /// Checks whether there are any more documents for the cursor to yield.
     pub fn has_next(&mut self) -> Result<bool> {
         self.inner.has_next().chain("cursor error")
+    }
+
+    /// Transforms and tries to deserialize a single document.
+    fn transform_and_deserialize_one(&mut self, doc: Document) -> Result<T> {
+        (self.transform)(doc).and_then(|b| from_bson(b).map_err(From::from))
+    }
+
+    /// Transforms and tries to deserialize a vector of documents.
+    fn transform_and_deserialize_many(&mut self, docs: Vec<Document>) -> Result<Vec<T>> {
+        docs.into_iter()
+            .map(|doc| self.transform_and_deserialize_one(doc))
+            .collect()
     }
 }
 
@@ -45,6 +71,7 @@ impl<T> From<mongodb::cursor::Cursor> for Cursor<T> where T: for<'a> Deserialize
     fn from(cursor: mongodb::cursor::Cursor) -> Self {
         Cursor {
             inner: cursor,
+            transform: |doc| Ok(doc.into()),
             _marker: PhantomData,
         }
     }
@@ -56,7 +83,11 @@ impl<T> Iterator for Cursor<T> where T: for<'a> Deserialize<'a> {
     fn next(&mut self) -> Option<Self::Item> {
         self.inner
             .next()
-            .map(|r| r.chain("can't step Cursor").and_then(deserialize_document))
+            .map(|result| {
+                result
+                    .chain("can't step Cursor")
+                    .and_then(|doc| self.transform_and_deserialize_one(doc))
+            })
     }
 }
 
