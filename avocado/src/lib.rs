0c93c8089c12eaa858635993a2c008de20daadf0
Árpád Goretity <h2co3@h2co3.org>
2019-01-02 12:24:12 +0100

    Release 0.2.0

--- avocado/src/lib.rs
+++ avocado/src/lib.rs
@@ -574,7 +574,7 @@
 //! * `raw_uuid` (default): augments the [`Uid`](uid/struct.Uid.html) type
 //!   with convenience methods for working with UUID-based entity/document IDs.
 
-#![doc(html_root_url = "https://docs.rs/avocado/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/avocado/0.2.0")]
 #![deny(missing_debug_implementations, missing_copy_implementations,
         trivial_casts, trivial_numeric_casts,
         unsafe_code,
